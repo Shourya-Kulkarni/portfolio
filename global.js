939e141d9e7d6c4641621cd2e5fab22400ed5ded
Shourya-Kulkarni <sdkulkarni@ucsd.edu>
2026-04-15 15:40:22 -0700

    Finished step 4
    edited style.css and global.js to edit website

--- global.js
+++ global.js
@@ -34,3 +34,25 @@ for (let p of pages) {
 
   nav.append(a);
 }
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `<label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+let select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+if ('colorScheme' in localStorage) {
+  select.value = localStorage.colorScheme;
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+}
