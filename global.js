62177fd59ed4e43c704cc4eb37b859f7f82595d1
Shourya-Kulkarni <sdkulkarni@ucsd.edu>
2026-04-15 15:26:41 -0700

    Step 3
    Edited html files and global.js

--- global.js
+++ global.js
@@ -3,10 +3,34 @@ console.log('IT\'S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-let navLinks = $$("nav a");
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/";
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/Shourya-Kulkarni', title: 'GitHub' },
+];
 
-currentLink?.classList.add('current');
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
+}
