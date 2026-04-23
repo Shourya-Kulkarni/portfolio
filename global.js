a0d9b2a9e7bd0fa5dc80e5deb1e5ac3f951a3168
Shourya-Kulkarni <sdkulkarni@ucsd.edu>
2026-04-23 15:02:41 -0700

    Completed step 3

--- global.js
+++ global.js
@@ -82,3 +82,6 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.append(article);
   }
 }
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
