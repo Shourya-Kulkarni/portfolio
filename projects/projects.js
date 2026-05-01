3f440f2861b826bb42ff7e3e72447edf532b6de7
Shourya-Kulkarni <sdkulkarni@ucsd.edu>
2026-05-01 12:26:03 -0700

    steps 1- 1.4

--- projects/projects.js
+++ projects/projects.js
@@ -5,3 +5,23 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `${projects.length} Projects`;
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+let data = Object.fromEntries(rolledData);
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(Object.values(data));
+let arcs = arcData.map(d => arcGenerator(d));
+
+let colors = d3.schemeTableau10;
+
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
+});
