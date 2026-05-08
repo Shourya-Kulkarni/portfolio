95907e19a9f7bb270a08064e2ccf7096a513ecd4
Shourya-Kulkarni <sdkulkarni@ucsd.edu>
2026-05-07 18:44:25 -0700

    step 3

--- meta/main.js
+++ meta/main.js
@@ -29,49 +29,62 @@ function processCommits(data) {
         hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
         totalLines: lines.length,
       };
-
       Object.defineProperty(ret, 'lines', {
         value: lines,
         configurable: true,
         writable: true,
         enumerable: false,
       });
-
       return ret;
     });
 }
 
-let data = await loadData();
-let commits = processCommits(data);
+function renderTooltipContent(commit) {
+  const link = document.getElementById('commit-link');
+  const date = document.getElementById('commit-date');
+  const time = document.getElementById('commit-time');
+  const author = document.getElementById('commit-author');
+  const lines = document.getElementById('commit-lines');
+  if (Object.keys(commit).length === 0) return;
+  link.href = commit.url;
+  link.textContent = commit.id;
+  date.textContent = commit.datetime?.toLocaleString('en', { dateStyle: 'full' });
+  time.textContent = commit.datetime?.toLocaleString('en', { timeStyle: 'short' });
+  author.textContent = commit.author;
+  lines.textContent = commit.totalLines;
+}
+
+function updateTooltipVisibility(isVisible) {
+  const tooltip = document.getElementById('commit-tooltip');
+  tooltip.hidden = !isVisible;
+}
+
+function updateTooltipPosition(event) {
+  const tooltip = document.getElementById('commit-tooltip');
+  tooltip.style.left = `${event.clientX}px`;
+  tooltip.style.top = `${event.clientY}px`;
+}
 
 function renderCommitInfo(data, commits) {
   const dl = d3.select('#stats').append('dl').attr('class', 'stats');
-
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append('dd').text(data.length);
-
   dl.append('dt').text('Total commits');
   dl.append('dd').text(commits.length);
-
   dl.append('dt').text('Number of files');
   dl.append('dd').text(d3.group(data, d => d.file).size);
-
   dl.append('dt').text('Maximum depth');
   dl.append('dd').text(d3.max(data, d => d.depth));
-
   dl.append('dt').text('Longest line');
   dl.append('dd').text(d3.max(data, d => d.length));
-
   dl.append('dt').text('Average line length');
   dl.append('dd').text(Math.round(d3.mean(data, d => d.length)));
 }
-renderCommitInfo(data, commits);
 
 function renderScatterPlot(data, commits) {
   const width = 1000;
   const height = 600;
   const margin = { top: 10, right: 10, bottom: 30, left: 20 };
-
   const usableArea = {
     top: margin.top,
     right: width - margin.right,
@@ -99,24 +112,20 @@ function renderScatterPlot(data, commits) {
     .range([usableArea.bottom, usableArea.top]);
 
   const gridlines = svg
-  .append('g')
-  .attr('class', 'gridlines')
-  .attr('transform', `translate(${usableArea.left}, 0)`);
-
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
   gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
 
   const xAxis = d3.axisBottom(xScale);
-  const yAxis = d3
-    .axisLeft(yScale)
+  const yAxis = d3.axisLeft(yScale)
     .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
 
-  svg
-    .append('g')
+  svg.append('g')
     .attr('transform', `translate(0, ${usableArea.bottom})`)
     .call(xAxis);
 
-  svg
-    .append('g')
+  svg.append('g')
     .attr('transform', `translate(${usableArea.left}, 0)`)
     .call(yAxis);
 
@@ -129,6 +138,18 @@ function renderScatterPlot(data, commits) {
     .attr('cx', (d) => xScale(d.datetime))
     .attr('cy', (d) => yScale(d.hourFrac))
     .attr('r', 5)
-    .attr('fill', 'steelblue');
+    .attr('fill', 'steelblue')
+    .on('mouseenter', (event, commit) => {
+      renderTooltipContent(commit);
+      updateTooltipVisibility(true);
+      updateTooltipPosition(event);
+    })
+    .on('mouseleave', () => {
+      updateTooltipVisibility(false);
+    });
 }
+
+let data = await loadData();
+let commits = processCommits(data);
+renderCommitInfo(data, commits);
 renderScatterPlot(data, commits);
