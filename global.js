ec1645dca3c4f08a59d019166c1dd34e61c5d54c
Shourya-Kulkarni <sdkulkarni@ucsd.edu>
2026-05-07 17:56:02 -0700

    Steps 0-1

--- global.js
+++ global.js
@@ -12,6 +12,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/Shourya-Kulkarni', title: 'GitHub' },
 ];
 
